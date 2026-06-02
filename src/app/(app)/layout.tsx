bc0d1255bb879c87e53df61577f2e60a009468f7
Deploy <deploy@fieldserviceit.com>
2026-06-02 11:43:49 -0400

    Stop forcing app routes dynamic

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,5 @@
 import { AppLayout } from '../../components/layout/AppLayout';
 
-export const dynamic = 'force-dynamic';
-
 export default function AppRouteLayout({ children }: { children: React.ReactNode }) {
   return <AppLayout>{children}</AppLayout>;
 }
